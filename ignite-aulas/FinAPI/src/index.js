be6d9cb23f13155a346556a4a0ef58dbae87b93a
Lucas de Araujo <araujolucas97@outlook.com>
2022-08-14 14:56:12 -0300

    [dev] fin api

--- ignite-aulas/FinAPI/src/index.js
+++ ignite-aulas/FinAPI/src/index.js
@@ -6,6 +6,20 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return res.status(400).json({ error: 'Customer not found' });
+  }
+
+  req.customer = customer;
+
+  return next();
+}
+
 app.post('/account', (req, res) => {
   const { cpf, name } = req.body;
 
@@ -22,14 +36,8 @@ app.post('/account', (req, res) => {
   return res.status(201).send(customers);
 });
 
-app.get('/statement/:cpf', (req, res) => {
-  const { cpf } = req.params;
-
-  const customer = customers.find(customer => customer.cpf === cpf);
-  if (!customer) {
-    return res.status(400).json({ error: 'Customer not found' });
-  }
-
+app.get('/statement', verifyExistsAccountCPF, (req, res) => {
+  const { customer } = req
   return res.status(200).send(customer.statement);
 });
 
