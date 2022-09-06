9938c66b35ba1a6a54848b93d6b604cddcdbfc74
Lucas <lucas@MacBook-Pro-de-Lucas.local>
2022-09-06 00:08:49 -0300

    [Dev] FinAPI - Chapter I - Módulo 2 - finalizado

--- ignite-aulas/FinAPI/src/index.js
+++ ignite-aulas/FinAPI/src/index.js
@@ -20,6 +20,18 @@ function verifyExistsAccountCPF(req, res, next) {
   return next();
 }
 
+function getBalance(statement){
+  const balance = statement.reduce((acc,operation) => {
+    if(operation.type === 'credit'){
+      return acc + operation.amount;
+    }else{
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
 app.post('/account', (req, res) => {
   const { cpf, name } = req.body;
 
@@ -41,5 +53,86 @@ app.get('/statement', verifyExistsAccountCPF, (req, res) => {
   return res.status(200).send(customer.statement);
 });
 
+app.post('/deposit', verifyExistsAccountCPF, (req, res) => {
+  const { description, amount } = req.body;
+
+  const {customer} = req;
+
+  const statementOperation ={ 
+    description,
+    amount,
+    create_at: new Date(),
+    type: 'credit'
+  }
+
+  customer.statement.push(statementOperation)
+
+  return res.status(201).send();
+});
+
+app.post('withdraw', verifyExistsAccountCPF, (req, res) =>{
+  const { amount } = req.body;
+  const { customer } = req;
+
+  const balance = getBalance(customer.statement);
+
+  if(balance < amount){
+    return res.status(400).json({error: 'Insufficient funds!'})
+  }
+
+  const statementOperation ={ 
+    amount,
+    create_at: new Date(),
+    type: 'debit'
+  }
+
+  customer.statement.push(statementOperation)
+  
+  return res.status(201).send()
+})
+
+app.get('/statement/date', verifyExistsAccountCPF, (req,res) => {
+  const {customer} = req;
+  const {date} = req.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter((statement) => 
+  statement.create_at.toDateString() === 
+  new date (dateFormat).toDateString())
+
+  return res.json(statement);
+})
+
+app.put('/account', verifyExistsAccountCPF,(req,res) => {
+  const {name} = req.body;
+  const {customer} = req;
+
+  customer.name = name;
+
+  return res.status(201).send();
+})
+
+app.get('/account', verifyExistsAccountCPF, (req,res) => {
+  const {customer} = req;
+
+  return res.json(customer);
+})
+
+app.delete('/account', verifyExistsAccountCPF, (req,res) => {
+  const {customer} = req;
+
+  customer.splice(customer, 1)
+
+  return res.status(200).json(customers);
+})
+
+app.get('/balance', verifyExistsAccountCPF, (req, res) =>{
+  const {customer} = require;
+
+  const balance = getBalance(customer.statement)
+
+  return res.json(balance);
+})
 
 app.listen(3333);
